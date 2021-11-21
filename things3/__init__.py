d76f83e287a6f1c9eda49c2b2102a9418dd9b4c8
Alexander Willner <a.willner@ciss.de>
2021-11-21 13:29:40 +0100

    version 2.8.0

--- things3/__init__.py
+++ things3/__init__.py
@@ -2,10 +2,10 @@
 
 
 __author__ = "Alexander Willner"
-__copyright__ = "2020 Alexander Willner"
+__copyright__ = "2021 Alexander Willner"
 __credits__ = ["Alexander Willner"]
 __license__ = "Apache License 2.0"
-__version__ = "2.7.2"
+__version__ = "2.8.0"
 __maintainer__ = "Alexander Willner"
 __email__ = "alex@willner.ws"
 __status__ = "Development"
